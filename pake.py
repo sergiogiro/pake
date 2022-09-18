0fb7d949ac56c31d6a874eb612f6b7d90846ddbc
Sergio Giro <sergio.giro.ar@gmail.com>
2022-09-18 15:44:41 +0100

    Add needs_update

--- pake.py
+++ pake.py
@@ -1,5 +1,6 @@
 import abc
 import enum
+from collections import defaultdict
 from glob import glob
 from dataclasses import dataclass
 import os
@@ -10,28 +11,20 @@ from typing import Callable, Generic, List, NewType, Optional, Type, TypeVar, Un
 T = TypeVar("T")
 
 
+@dataclass(frozen=True)
 class Dependable:
-    def needs_update() -> bool:
-        return True
+    pass
 
 
+@dataclass(frozen=True)
 class AtomicDependable(Dependable):
     pass
 
 
-@dataclass
+@dataclass(frozen=True)
 class FileDependable(AtomicDependable):
     filename: str
 
-
-@dataclass
-class Output:
-    pass
-    
-
-@dataclass
-class FileOutput(Output):
-    filename: str
 
 @dataclass(frozen=True)
 class Dependency(Generic[T]):
@@ -41,14 +34,41 @@ class Dependency(Generic[T]):
     is_optional: bool = False
 
 
-@dataclass
+@dataclass(frozen=True)
+class Output(Dependable):
+    pass
+
+
+@dataclass(frozen=True)
+class FileOutput(Output):
+    filename: str
+
+
+
+def _default_outcome_needs_update(
+    output: Optional[Output],
+    dependency: Optional[Dependency] = None,
+    dependable: Optional[AtomicDependable] = None,
+) -> bool:
+    # Outcomes without outputs always trigger.
+    if output is None:
+        return True
+    return output.needs_update(dependency, dependable)
+
+
+@dataclass(frozen=True)
 class Outcome(Generic[T]):
     name: str
-    output_type: T
+    output_type: Optional[T]
     outputs_from_dependables: Callable[
         [dict[Dependency, list[AtomicDependable]]],
-        list[T],
+        dict[Output, dict[Dependency, list[AtomicDependable, bool]]]
+,
     ]
+    needs_update: Callable[[Optional[T], Optional[Dependency], Optional[AtomicDependable]], bool] = _default_outcome_needs_update
+
+
+OutputMap = dict[Output, dict[Outcome, dict[Dependency, list[Dependable, bool]]]]
 
 
 Executable = Callable[[], None]
@@ -60,12 +80,19 @@ DependenciesExpander = Callable[
 ]
 
 
+def _default_rule_needs_update(self, output_map: OutputMap, rule_args: T, outcome: Optional[Outcome], output: Optional[Output], has_outcomes: bool, has_dependencies: bool, has_outputs_needing_update: bool):
+    # Rules without outcomes or dependencies always trigger.
+    return (not has_outcomes) or (not has_dependencies) or has_outputs_needing_update
+
+
 @dataclass
 class Rule(Generic[T]):
     name: str
     dependencies: Callable[[T], list[Dependency]]
     outcomes: Callable[[T], list[Outcome]]
     executable: Callable[[T], Executable]
+    filter_for_needing_update: bool = False
+    needs_update = _default_rule_needs_update
 
     def artifact(
         self,
@@ -89,10 +116,48 @@ class Artifact(Generic[T], Dependable):
         self.rule = rule
         self.dependencies_map = dependencies_map
         self.expanded_dependencies_map = {k: k.expand(v) for k, v in self.dependencies_map.items()}
-        self.executable = self.rule.executable(self.expanded_dependencies_map)
-        self.outputs = [puts for comes in self.rule.outcomes(rule_args) for puts in comes.outputs_from_dependables(self.expanded_dependencies_map)]
+        self.outcomes = self.rule.outcomes(rule_args)
+        self.output_map = defaultdict(
+                lambda: defaultdict(
+                    lambda: defaultdict(
+                        lambda: set()
+                    )
+                )
+            )
+        self.has_outcomes = False
+        self.dependencies = False
+        self.has_outputs_needing_update = False
+        for come in self.rule.outcomes(rule_args):
+            self.has_outcomes = True
+            for put, ency_to_ables in come.outputs_from_dependables(
+                    self.expanded_dependencies_map
+            ).items():
+                for ency, ables in ency_to_ables.items():
+                    self.has_dependencies = True
+                    for able in ables:
+                        nu = come.needs_update(put, ency, able)
+                        self.has_outputs_needing_update = self.has_outputs_needing_update or nu
+                        if nu or not rule.filter_for_needing_update:
+                            self.output_map[put][come][ency].add(
+                                (able, nu)
+                            )
+        # TODO: if an outcome doesn't have outputs, call needs_update with
+        # output=None for each dependency/dependable (if there are no
+        # dependencies/dependables, use None for those).
+        self.needs_update = lambda outcome, output: self.rule.needs_update(
+                self.output_map,
+                self.rule_args,
+                outcome,
+                output,
+                self.has_outcomes,
+                self.has_dependencies,
+                has_outputs_needing_update,
+        )
+        self.executable = self.rule.executable(self.output_map)
 
-GlobExpr = NewType("Glob", "str")
+
+
+GlobExpr = NewType("Glob", str)
 
 
 class ObjsFromCsArgs(enum.Enum):
@@ -106,10 +171,10 @@ GlobDependency = Dependency(
     dependable_type=GlobExpr,
     expand=lambda x: [FileDependable(f) for f in glob(x)],
 )
-FileDependency = Dependency(
+FileNameDependency = Dependency(
     name="file",
     dependable_type=str,
-    expand=lambda x: [FileDependable(x)],
+    expand=lambda x: [FileDependable(x)]
 )
 FileNameListDependency = Dependency(
     name="files",
@@ -128,38 +193,31 @@ def objs_from_cs_dependencies(args: ObjsFromCsArgs) -> list[Dependency]:
         return [FileNameListDependency]
 
 
-def objs_from_cs_outcomes(args: ObjsFromCsArgs) -> list[Outcome]:
+def obj_files_outputs_from_dependables(deps_to_ables: dict[Dependency, list[AtomicDependable]]):
+    print("Deps to ables:", deps_to_ables)
     def extension_to_o(path):
         spath = path.rsplit(".", 1)
         if len(spath) < 2:
-            raise ValueError(f"Path doesn't have an extension: [{path}]")
+            raise ValueError(f"Path doesn't have an extension: [{spath}]")
         p, e = spath
         if os.sep in e:
             raise ValueError(
                 "Path doesn't have an extension (only dot is at the left "
-                f"of a {os.sep}): [{path}]"
+                f"of a {os.sep}): [{spath}]"
             )
         return p + ".o"
 
-    if args in (ObjsFromCsArgs.globExpr, ObjsFromCsArgs.fileNameList):
-        return [
-            Outcome(
-                name="obj_files",
-                output_type=FileOutput,
-                outputs_from_dependables=lambda d: [
-                    FileOutput(filename=extension_to_o(f.filename))
-                    for _, dables in d.items()
-                    for f in dables
-                ]
-            )
-        ]
+    return { FileOutput(extension_to_o(able.filename)): { ency: set((able,)) }
+             for ency in deps_to_ables
+             for able in deps_to_ables[ency] }
+
+def objs_from_cs_outcomes(args: ObjsFromCsArgs) -> list[Outcome]:
     return [
         Outcome(
-            name="obj_file",
+            name="obj_files",
             output_type=FileOutput,
-            outputs_from_dependables=lambda d: [
-                FileOutput(filename=extension_to_o(list(d.values())[0].filename))
-            ]
+            outputs_from_dependables=obj_files_outputs_from_dependables,
+            needs_update=lambda out, ency, able: True
         )
     ]
 
@@ -170,10 +228,17 @@ def shell_executable(command: list[str]) -> Executable:
     return execute
 
 
-def objs_from_cs_executable(deps: dict[Dependency, list[AtomicDependable]]):
-    print("Deps:", deps)
+def objs_from_cs_executable(output_map: OutputMap):
+    ables = [
+        ables_and_needs[0]
+        for put in output_map
+        for come in output_map[put]
+        for ency in output_map[put][come]
+        for ables_and_needs in output_map[put][come][ency]
+    ]
+
     return shell_executable(
-        ["gcc", "-c"] + [f.filename for ds in deps.values() for f in ds]
+        ["gcc", "-c"] + [able.filename for able in ables]
     )
 
 
@@ -187,4 +252,4 @@ objs_from_cs = Rule(
 
 artifact = objs_from_cs.artifact("this_dir_objs", ObjsFromCsArgs.globExpr, {GlobDependency: "*.c"})
 artifact.executable()
-print("Outputs", artifact.outputs)
+print("Outputs", artifact.output_map.keys())
